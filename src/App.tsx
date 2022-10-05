522ac36b8a3cfa6631b37558a8d5e2ba52011bab
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-10-05 10:43:02 -0400

    added Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 
 // common components
 import Header from './components/header';
+import Footer from './components/footer';
 
 // styles and fonts
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
 
+      {/* Main Content */}
       <div className="container">
       <h1>Hello, World!</h1>
       </div>
+
+
+      <Footer></Footer>
+
     </div>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer()
+{
+    return(
+        <nav className="navbar fixed-bottom bg-light">
+            <div className="container-fluid">
+              <h4>All Rights Reserved. Portfolio Site. &copy; Copyright 2022</h4>
+            </div>
+          </nav>
+    )
+}
+
+export default Footer;
