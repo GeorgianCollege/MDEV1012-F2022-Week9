dd0bab9fdd42454a928c8a438b18f5d917a59979
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-10-12 08:33:01 -0400

    added remaining pages

---
+++ src/content/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About()
+{
+    return(
+        <div>
+            <h1 className="mb-5">About Us</h1>
+            <hr />
+        </div>
+    )
+}
+
+export default About;
---
+++ src/content/apps.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Apps()
+{
+    return(
+        <div>
+            <h1 className="mb-5">Our Apps</h1>
+            <hr />
+        </div>
+    )
+}
+
+export default Apps;
