578b83da55fa3111ab21207348a0d742169959c1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-09 09:22:40 -0500

    added Hero Button to Home page

--- src/content/home.tsx
+++ src/content/home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import User from '../models/User';
 
@@ -8,6 +9,10 @@ function Home(props: User)
         <div>
             <h1 className="mb-5">Welcome to our Site, {props.displayName}</h1>
             <hr />
+
+            <Link to={"/about"}>
+                <button id="heroButton" className="btn btn-primary btn-lg">Learn more About Us! <i className="fa-solid fa-people-arrows fa-lg"></i></button>
+            </Link>
         </div>
     )
 }
